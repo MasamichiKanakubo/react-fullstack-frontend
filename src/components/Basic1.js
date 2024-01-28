07c060103cbf81018401b3f8a4fef91b57bb4118
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-01-28 14:00:31 +0900

    feat: learn to props

---
+++ src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1 = (props) => {
+  return (
+    <div>
+      <h1>hello { props.name }</h1>
+      <h1>hello react2</h1>
+    </div>
+  )
+}
+
+export default Basic1
